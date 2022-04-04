23981148045607882c876aab87c2adba686f676f
SP3EDY78 <54348069+SP3EDY78@users.noreply.github.com>
2022-04-04 10:03:34 +1000

    NA

---
+++ coercion.js
@@ -0,0 +1,31 @@
+/* let a = 7;
+let b = '6';
+b = parseInt(b, 10);
+
+let c = a + b;
+console.log('Answer: ' + c);
+
+let d = parseInt('bob', 10);
+console.log(d);
+
+let e = isNaN(d);
+console.log(e); */
+
+// let f = true;
+let f = false;
+console.log(typeof f);
+console.log(f);
+
+// welp that didnt work :(
+/* if (f = true) {
+    f = '1';
+}
+if (f = false) {
+    f = '0';
+    }
+console.log(f);
+console.log(typeof f); */
+
+f = true ? 1 : 0;
+console.log(typeof f);
+console.log(f);
