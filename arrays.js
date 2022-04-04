c3ac866f9d7ffa02480cd303c74d447d32c2386d
SP3EDY78 <54348069+SP3EDY78@users.noreply.github.com>
2022-04-04 10:36:55 +1000

    NA

---
+++ arrays.js
@@ -0,0 +1,21 @@
+let a = [4, 8, 15, 16, 42];
+let b = ['david', 'eddie', 'alex', 'michael'];
+
+console.log(a);
+/* 
+console.log(a[0]);
+console.log(a[1]);
+console.log(a);
+ */
+
+a[0] = 7;
+console.log(a);
+console.log(typeof a);
+
+let c = [4, 'alex', true];
+console.log(c);
+
+//undefined :D
+// console.log(b[4]);
+
+console.log(a.length); //actual number not zero based
